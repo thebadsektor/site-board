9e960160677ed147f9ec6e187a5cda849a6047a5
GonzaloDanielBenitez And <gonzalo.daniel.tech@gmail.com>
2023-04-13 09:12:05 -0500

    optimize camera animation

--- src/components/Scene/Camera.jsx
+++ src/components/Scene/Camera.jsx
@@ -3,7 +3,7 @@ import {PerspectiveCamera} from '@react-three/drei'
 import * as THREE from 'three'
 import {useFrame} from '@react-three/fiber'
 import {useZustand} from '../../store/useZustand'
-import {LERP_ALPHA, TOLERANCE_DISTANCE} from '../../utils/constants'
+import {CAMERA_INIT_POS, LERP_ALPHA, TOLERANCE_DISTANCE} from '../../utils/constants'
 import {customDebug} from '../../utils/custom.debug'
 
 
@@ -21,16 +21,19 @@ export const Camera = () => {
       return
     }
     const {height} = billboardDimensions
-    const cameraCurPosVec3 = state.camera.position
     cameraDesPosVec3.set(cameraDesPos[0], cameraDesPos[1] + (height / 2), cameraDesPos[2])
     billboardDesPosVec3.set(billboardDesPos[0], billboardDesPos[1] + (height / 2), billboardDesPos[2])
     state.camera.position.lerp(cameraDesPosVec3, LERP_ALPHA)
     state.camera.lookAt(billboardDesPosVec3)
-    const distance = cameraDesPosVec3.sub(cameraCurPosVec3).length()
+    const distance = cameraDesPosVec3.sub(state.camera.position).length()
+
     if (distance > TOLERANCE_DISTANCE) {
       customDebug().log('Camera#useFrame: camera moving')
-    } else {
+      stopped = false
+    } else if (!stopped) {
       customDebug().log('Camera#useFrame: camera stopped')
+      cameraCurPosVec3.copy(state.camera.position)
+      stopped = true
     }
   })
 
@@ -43,6 +46,7 @@ export const Camera = () => {
 }
 
 
-// const cameraCurPosVec3 = new THREE.Vector3()
+const cameraCurPosVec3 = new THREE.Vector3(CAMERA_INIT_POS[0], CAMERA_INIT_POS[1], CAMERA_INIT_POS[2])
 const cameraDesPosVec3 = new THREE.Vector3()
 const billboardDesPosVec3 = new THREE.Vector3()
+let stopped = true
