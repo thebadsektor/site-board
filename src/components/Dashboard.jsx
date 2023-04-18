8443573673565a00b424519bd83c181fcfe66622
GonzaloDanielBenitez And <gonzalo.daniel.tech@gmail.com>
2023-04-17 22:46:19 -0500

    decrease real time visitors count

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -45,7 +45,7 @@ export const Dashboard = () => {
         // eslint-disable-next-line no-unused-vars
         const newRealtimeVisitors = await getRealtimeVisitors(domain)
         // setRealtimeVisitors(newRealtimeVisitors)
-        setRealtimeVisitors(10)
+        setRealtimeVisitors(4)
       }
     }
 
