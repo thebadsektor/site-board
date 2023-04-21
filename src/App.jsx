d9b7f1729973df6dd1a1d6c559d0afca5ad7baa6
GonzaloDanielBenitez And <gonzalo.daniel.tech@gmail.com>
2023-04-21 13:54:09 -0500

    add onRedirectCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, {useEffect} from 'react'
 import {useControls} from 'leva'
 import {Alert} from './components/Utils/Alert'
 import {Confirm} from './components/Utils/Confirm'
@@ -9,12 +9,20 @@ import {BillboardPage} from './components/MBoard/Scene/BillboardPage'
 import {MBoard} from './components/MBoard/MBoard'
 import {Home} from './components/Home'
 import {Loading} from './components/Utils/Loading'
+import {useAuth0} from '@auth0/auth0-react'
+import {useZustand} from './store/useZustand'
 
 
 const App = () => {
   const {fullScreen} = useControls({
     fullScreen: {value: false, label: 'Full Screen'},
   })
+  const {setIsLoading} = useZustand()
+  const {isLoading} = useAuth0()
+
+  useEffect(() => {
+    setIsLoading(isLoading)
+  }, [isLoading, setIsLoading])
 
   return (
     <div className='relative flex flex-col w-screen h-screen'>
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserHistory} from 'history'
 import App from './App'
 import authConfig from './backend/auth.config.json'
 import './index.css'
 import {Auth0Provider} from '@auth0/auth0-react'
 
 
+const browserHistory = createBrowserHistory()
+
+
+const onRedirectCallback = (appState) => {
+  browserHistory.push(
+    appState && appState.returnTo ? appState.returnTo : window.location.pathname,
+  )
+}
+
+
 const providerConfig = {
   domain: authConfig.domain,
   clientId: authConfig.clientId,
+  onRedirectCallback,
   authorizationParams: {
     redirect_uri: window.location.origin,
     ...(authConfig.audience ? {audience: authConfig.audience} : null),
