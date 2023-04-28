69b0248c45bda55fff385c3bb5668a711e4b48e3
GonzaloDanielBenitez And <gonzalo.daniel.tech@gmail.com>
2023-04-27 21:52:58 -0500

    add this script to the head section of the page you want to track

--- src/components/Plausible/Snippet.jsx
+++ src/components/Plausible/Snippet.jsx
@@ -13,7 +13,7 @@ export const Snippet = () => {
 
   return (
     <>
-      <div>Code Snippet</div>
+      <div>Add this script to the head section of the page you want to track</div>
       <div>{domain && `<script defer data-domain="${domain}" src="https://plausible.io/js/script.js"></script>`}</div>
       <button
         className='pl-2 pr-2 border-2 rounded'
