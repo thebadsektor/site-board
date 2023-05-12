09e52d642477b04ab005ef47a2764144420ec81d
GonzaloDanielBenitez And <gonzalo.daniel.tech@gmail.com>
2023-05-12 06:07:14 -0500

    update auth0 config

--- src/backend/auth.config.js
+++ src/backend/auth.config.js
@@ -3,11 +3,11 @@ import {IS_DEV_MODE} from '../utils/constants'
 
 export const authConfig = IS_DEV_MODE ?
 {
-  domain: 'dev-ku6ur1mt530rft6t.us.auth0.com',
-  clientId: 'vnaVUHcNstsBWh2cFAZs2btEqbaIX9DO',
-  audience: 'https://dev-ku6ur1mt530rft6t.us.auth0.com/api/v2/',
+  domain: 'dev-rqzc417axamwy78e.us.auth0.com',
+  clientId: '7z1MHCx4k4ZUMfpdUDFEVluc6D6NXXsY',
+  audience: 'https://dev-rqzc417axamwy78e.us.auth0.com/api/v2/',
 } : {
-  domain: 'dev-ku6ur1mt530rft6t.us.auth0.com',
-  clientId: '4UWwNqhkVMrVnwas9YVpOr1eLcqeyYBJ',
-  audience: 'https://dev-ku6ur1mt530rft6t.us.auth0.com/api/v2/',
+  domain: 'dev-rqzc417axamwy78e.us.auth0.com',
+  clientId: 'IwW12s391bnpX47wK08TaVew5S4IXhXy',
+  audience: 'https://dev-rqzc417axamwy78e.us.auth0.com/api/v2/',
 }
