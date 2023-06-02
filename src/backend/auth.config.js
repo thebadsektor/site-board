1b77aa33a42d99420d41fb97570efddcce0b014f
GonzaloDanielBenitez And <gonzalo.daniel.tech@gmail.com>
2023-06-02 09:07:47 -0500

    update auto config

--- src/backend/auth.config.js
+++ src/backend/auth.config.js
@@ -4,10 +4,10 @@ import {IS_DEV_MODE} from '../utils/constants'
 export const authConfig = IS_DEV_MODE ?
   {
     domain: 'dev-ku6ur1mt530rft6t.us.auth0.com',
-    clientId: 'vnaVUHcNstsBWh2cFAZs2btEqbaIX9DO',
+    clientId: 'IT4ZO5LK6J8xgb6XUaf9FSP4bzJhbnkv',
     audience: 'https://dev-ku6ur1mt530rft6t.us.auth0.com/api/v2/',
   } : {
     domain: 'dev-ku6ur1mt530rft6t.us.auth0.com',
-    clientId: '4UWwNqhkVMrVnwas9YVpOr1eLcqeyYBJ',
+    clientId: 'Aqx55ue1rpneXpfKOs9VWMYZQUh65hwJ',
     audience: 'https://dev-ku6ur1mt530rft6t.us.auth0.com/api/v2/',
   }
